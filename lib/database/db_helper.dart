8ea73219960e34e2ebbd905eeed6604202652cdc
AR <53275317+AAR072@users.noreply.github.com>
2025-02-01 03:16:10 -0500

    feat: backend

---
+++ lib/database/db_helper.dart
@@ -0,0 +1,219 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:benchy/database/models.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('benchy.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    // Get a list of all table names
+    List<Map<String, dynamic>> tables = await db.rawQuery(
+      "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'");
+
+    // Drop each table
+    for (var table in tables) {
+    String tableName = table['name'];
+    await db.execute("DROP TABLE IF EXISTS $tableName");
+  }
+    // Create Workouts table
+    await db.execute('''
+      CREATE TABLE Workouts (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      date INTEGER NOT NULL,
+      duration INTEGER NOT NULL,
+      notes TEXT,
+      volume REAL,
+      rating INTEGER,
+      calories_burned INTEGER
+      )
+      ''');
+
+    // Create Movements table
+    await db.execute('''
+      CREATE TABLE Movements (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      type TEXT NOT NULL,
+      one_rep_max REAL,
+      muscleGroups TEXT,
+      instructions TEXT,
+      imageUrl TEXT,
+      maxWeight REAL,
+      maxSessionVolume REAL,
+      maxSetVolume REAL,
+      equipment TEXT,
+      completion_count INTEGER
+      )
+      ''');
+
+    // Create Maxes table
+    await db.execute('''
+      CREATE TABLE Maxes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      movement_id INTEGER NOT NULL,
+      metric TEXT NOT NULL,
+      value REAL NOT NULL,
+      date INTEGER NOT NULL,
+      FOREIGN KEY (movement_id) REFERENCES Movements (id) ON DELETE CASCADE
+      )
+      ''');
+
+    // Create Exercise table
+    await db.execute('''
+      CREATE TABLE Exercise (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      category TEXT NOT NULL,
+      movement_id INTEGER NOT NULL,
+      workout_id INTEGER NOT NULL,
+      order_index INTEGER NOT NULL,
+      restTime INTEGER,
+      notes TEXT,
+      date INTEGER NOT NULL,
+      volume REAL,
+      FOREIGN KEY (movement_id) REFERENCES Movements (id) ON DELETE CASCADE,
+      FOREIGN KEY (workout_id) REFERENCES Workouts (id) ON DELETE CASCADE
+      )
+      ''');
+
+    // Create Set table
+    await db.execute('''
+      CREATE TABLE WorkoutSet(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      notes TEXT,
+      exercise_id INTEGER NOT NULL,
+      reps INTEGER,
+      weight REAL,
+      volume REAL,
+      time INTEGER,
+      distance REAL,
+      rpe INTEGER,
+      FOREIGN KEY (exercise_id) REFERENCES Exercise (id) ON DELETE CASCADE
+      )
+      ''');
+
+    // Create WorkoutTemplates table
+    await db.execute('''
+      CREATE TABLE WorkoutTemplates (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      notes TEXT,
+      exercises TEXT NOT NULL
+      )
+      ''');
+
+    // Create Goals table
+    await db.execute('''
+      CREATE TABLE Goals (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      movement_id INTEGER NOT NULL,
+      target_value REAL NOT NULL,
+      target_date INTEGER NOT NULL,
+      notes TEXT,
+      achieved INTEGER NOT NULL DEFAULT 0, -- 0 for false, 1 for true
+      FOREIGN KEY (movement_id) REFERENCES Movements (id) ON DELETE CASCADE
+      )
+      ''');
+
+    // Create BodyMeasurements table
+    await db.execute('''
+      CREATE TABLE BodyMeasurements (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      unit TEXT NOT NULL,
+      notes TEXT
+      )
+      ''');
+
+    // Create BodyMetrics table
+    await db.execute('''
+      CREATE TABLE BodyMetrics (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      measurement_id INTEGER NOT NULL,
+      value REAL NOT NULL,
+      date INTEGER NOT NULL,
+      notes TEXT,
+      FOREIGN KEY (measurement_id) REFERENCES BodyMeasurements (id) ON DELETE CASCADE
+      )
+      ''');
+  }
+  /// Inserts a [workout]
+  /// Example: 
+  /// ```dart
+  /// final workout = Workout(
+  /// id: null, // Auto-incremented by the database
+  /// title: 'Morning Workout',
+  /// date: DateTime.now().millisecondsSinceEpoch 
+  /// duration: 60,
+  /// notes: 'Great session!',
+  /// volume: 1500.0,
+  /// rating: 9,
+  /// caloriesBurned: 300,
+  /// );
+  /// final id = await DatabaseHelper.instance.insertWorkout(workout);
+  /// print('Inserted workout with ID: $id');
+  /// ```
+  Future<int> insertWorkout(Workout workout) async {
+    final db = await database;
+    return await db.insert('Workouts', workout.toMap());
+  }
+
+  /// Returns a [List] of all the user's [Workout]s 
+  /// Ex:
+  /// ```dart
+  /// final workouts = await DatabaseHelper.instance.getAllWorkouts();
+  /// workouts.forEach((workout) {
+  /// print('Workout: ${workout.title}, Date: ${workout.date}');
+  /// });
+  /// ```
+Future<List<Workout>> getAllWorkouts() async {
+  final db = await database;
+  final List<Map<String, dynamic>> maps = await db.query('Workouts');
+
+  return List.generate(maps.length, (i) {
+    return Workout(
+      id: maps[i]['id'],
+      title: maps[i]['title'],
+      date: DateTime.tryParse(maps[i]['date']) ?? DateTime(1970, 1, 1), // Fixed here
+      duration: maps[i]['duration'],
+      notes: maps[i]['notes'],
+      volume: maps[i]['volume'],
+      rating: maps[i]['rating'],
+      caloriesBurned: maps[i]['calories_burned'],
+    );
+  });
+}
+Future<void> deleteDatabaseFile() async {
+  final dbPath = await getDatabasesPath();
+  final path = join(dbPath, 'benchy.db');
+
+  // Close the existing database connection
+  if (_database != null) {
+    await _database!.close();
+    _database = null;
+  }
+
+  // Delete the database file
+  await deleteDatabase(path);
+  print('Database deleted at: $path');
+}
+}
---
+++ lib/database/models.dart
@@ -0,0 +1,414 @@
+import 'dart:convert'; // For JSON encoding/decoding
+
+class Workout {
+  final int? id;
+  final String title;
+  final DateTime date;
+  final int duration;
+  final String notes;
+  final double volume;
+  final int? rating;
+  final int caloriesBurned;
+
+/// The [duration] is in seconds
+/// The [volume] is in lbs 
+/// The [caloriesBurned] are 0 by default; 
+  Workout({
+    this.id,
+    this.rating,
+    required this.title,
+    required this.date,
+    required this.duration,
+    required this.volume,
+    this.notes = "",
+    this.caloriesBurned = 0,
+  });
+
+  // Convert a Workout object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'date': date.toIso8601String(),
+      'duration': duration,
+      'notes': notes,
+      'volume': volume,
+      'rating': rating,
+      'calories_burned': caloriesBurned,
+    };
+  }
+
+  // Create a Workout object from a Map
+  factory Workout.fromMap(Map<String, dynamic> map) {
+    return Workout(
+      id: map['id'],
+      title: map['title'],
+      date: DateTime.parse(map['date']),
+      duration: map['duration'],
+      notes: map['notes'],
+      volume: map['volume'],
+      rating: map['rating'],
+      caloriesBurned: map['calories_burned'],
+    );
+  }
+}
+
+class Movement {
+  final int? id;
+  final String name;
+  final String type;
+  final double oneRepMax;
+  final Map<String, dynamic> muscleGroups;
+  final String instructions;
+  final String imageUrl;
+  final double maxWeight;
+  final double maxSessionVolume;
+  final double maxSetVolume;
+  final String equipment;
+  final int completionCount;
+
+  Movement({
+    this.id,
+    required this.name,
+    required this.type,
+    required this.oneRepMax,
+    required this.muscleGroups,
+    required this.instructions,
+    required this.imageUrl,
+    required this.maxWeight,
+    required this.maxSessionVolume,
+    required this.maxSetVolume,
+    required this.equipment,
+    required this.completionCount,
+  });
+
+  // Convert a Movement object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'type': type,
+      'one_rep_max': oneRepMax,
+      'muscleGroups': jsonEncode(muscleGroups), // Convert JSON to string
+      'instructions': instructions,
+      'imageUrl': imageUrl,
+      'maxWeight': maxWeight,
+      'maxSessionVolume': maxSessionVolume,
+      'maxSetVolume': maxSetVolume,
+      'equipment': equipment,
+      'completion_count': completionCount,
+    };
+  }
+
+  // Create a Movement object from a Map
+  factory Movement.fromMap(Map<String, dynamic> map) {
+    return Movement(
+      id: map['id'],
+      name: map['name'],
+      type: map['type'],
+      oneRepMax: map['one_rep_max'],
+      muscleGroups: jsonDecode(map['muscleGroups']), // Convert string to JSON
+      instructions: map['instructions'],
+      imageUrl: map['imageUrl'],
+      maxWeight: map['maxWeight'],
+      maxSessionVolume: map['maxSessionVolume'],
+      maxSetVolume: map['maxSetVolume'],
+      equipment: map['equipment'],
+      completionCount: map['completion_count'],
+    );
+  }
+}
+
+class Maxes {
+  final int? id;
+  final int movementId;
+  final String metric;
+  final double value;
+  final DateTime date;
+
+  Maxes({
+    this.id,
+    required this.movementId,
+    required this.metric,
+    required this.value,
+    required this.date,
+  });
+
+  // Convert a Maxes object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'movement_id': movementId,
+      'metric': metric,
+      'value': value,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  // Create a Maxes object from a Map
+  factory Maxes.fromMap(Map<String, dynamic> map) {
+    return Maxes(
+      id: map['id'],
+      movementId: map['movement_id'],
+      metric: map['metric'],
+      value: map['value'],
+      date: DateTime.parse(map['date']),
+    );
+  }
+}
+
+class Exercise {
+  final int? id;
+  final String category;
+  final int movementId;
+  final int workoutId;
+  final int orderIndex;
+  final int restTime;
+  final String notes;
+  final DateTime date;
+  final double volume;
+
+  Exercise({
+    this.id,
+    required this.category,
+    required this.movementId,
+    required this.workoutId,
+    required this.orderIndex,
+    required this.restTime,
+    required this.notes,
+    required this.date,
+    required this.volume,
+  });
+
+  // Convert an Exercise object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'category': category,
+      'movement_id': movementId,
+      'workout_id': workoutId,
+      'order_index': orderIndex,
+      'restTime': restTime,
+      'notes': notes,
+      'date': date.toIso8601String(),
+      'volume': volume,
+    };
+  }
+
+  // Create an Exercise object from a Map
+  factory Exercise.fromMap(Map<String, dynamic> map) {
+    return Exercise(
+      id: map['id'],
+      category: map['category'],
+      movementId: map['movement_id'],
+      workoutId: map['workout_id'],
+      orderIndex: map['order_index'],
+      restTime: map['restTime'],
+      notes: map['notes'],
+      date: DateTime.parse(map['date']),
+      volume: map['volume'],
+    );
+  }
+}
+
+class WorkoutSet {
+  final int? id;
+  final String notes;
+  final int exerciseId;
+  final int reps;
+  final double weight;
+  final double volume;
+  final int time;
+  final double distance;
+  final int rpe;
+
+  WorkoutSet({
+    this.id,
+    required this.notes,
+    required this.exerciseId,
+    required this.reps,
+    required this.weight,
+    required this.volume,
+    required this.time,
+    required this.distance,
+    required this.rpe,
+  });
+
+  // Convert a Set object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'notes': notes,
+      'exercise_id': exerciseId,
+      'reps': reps,
+      'weight': weight,
+      'volume': volume,
+      'time': time,
+      'distance': distance,
+      'rpe': rpe,
+    };
+  }
+
+  // Create a Set object from a Map
+  factory WorkoutSet.fromMap(Map<String, dynamic> map) {
+    return WorkoutSet(
+      id: map['id'],
+      notes: map['notes'],
+      exerciseId: map['exercise_id'],
+      reps: map['reps'],
+      weight: map['weight'],
+      volume: map['volume'],
+      time: map['time'],
+      distance: map['distance'],
+      rpe: map['rpe'],
+    );
+  }
+}
+
+class WorkoutTemplates {
+  final int? id;
+  final String title;
+  final String notes;
+  final List<dynamic> exercises; // Array of exercise IDs with order, sets, reps, etc.
+
+  WorkoutTemplates({
+    this.id,
+    required this.title,
+    required this.notes,
+    required this.exercises,
+  });
+
+  // Convert a WorkoutTemplates object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'notes': notes,
+      'exercises': jsonEncode(exercises), // Convert JSON to string
+    };
+  }
+
+  // Create a WorkoutTemplates object from a Map
+  factory WorkoutTemplates.fromMap(Map<String, dynamic> map) {
+    return WorkoutTemplates(
+      id: map['id'],
+      title: map['title'],
+      notes: map['notes'],
+      exercises: jsonDecode(map['exercises']), // Convert string to JSON
+    );
+  }
+}
+
+class Goals {
+  final int? id;
+  final int movementId;
+  final double targetValue;
+  final DateTime targetDate;
+  final String notes;
+  final bool achieved;
+
+  Goals({
+    this.id,
+    required this.movementId,
+    required this.targetValue,
+    required this.targetDate,
+    required this.notes,
+    required this.achieved,
+  });
+
+  // Convert a Goals object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'movement_id': movementId,
+      'target_value': targetValue,
+      'target_date': targetDate.toIso8601String(),
+      'notes': notes,
+      'achieved': achieved ? 1 : 0, // Store as integer (1 for true, 0 for false)
+    };
+  }
+
+  // Create a Goals object from a Map
+  factory Goals.fromMap(Map<String, dynamic> map) {
+    return Goals(
+      id: map['id'],
+      movementId: map['movement_id'],
+      targetValue: map['target_value'],
+      targetDate: DateTime.parse(map['target_date']),
+      notes: map['notes'],
+      achieved: map['achieved'] == 1, // Convert integer back to boolean
+    );
+  }
+}
+
+class BodyMeasurements {
+  final int? id;
+  final String name;
+  final String unit;
+  final String notes;
+
+  BodyMeasurements({
+    this.id,
+    required this.name,
+    required this.unit,
+    required this.notes,
+  });
+
+  // Convert a BodyMeasurements object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'unit': unit,
+      'notes': notes,
+    };
+  }
+
+  // Create a BodyMeasurements object from a Map
+  factory BodyMeasurements.fromMap(Map<String, dynamic> map) {
+    return BodyMeasurements(
+      id: map['id'],
+      name: map['name'],
+      unit: map['unit'],
+      notes: map['notes'],
+    );
+  }
+}
+
+class BodyMetrics {
+  final int? id;
+  final int measurementId;
+  final double value;
+  final DateTime date;
+  final String notes;
+
+  BodyMetrics({
+    this.id,
+    required this.measurementId,
+    required this.value,
+    required this.date,
+    required this.notes,
+  });
+
+  // Convert a BodyMetrics object into a Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'measurement_id': measurementId,
+      'value': value,
+      'date': date.toIso8601String(),
+      'notes': notes,
+    };
+  }
+
+  // Create a BodyMetrics object from a Map
+  factory BodyMetrics.fromMap(Map<String, dynamic> map) {
+    return BodyMetrics(
+      id: map['id'],
+      measurementId: map['measurement_id'],
+      value: map['value'],
+      date: DateTime.parse(map['date']),
+      notes: map['notes'],
+    );
+  }
+}
