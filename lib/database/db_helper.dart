d1649fdc403b083d31ec845438359fb999db6585
AR <53275317+AAR072@users.noreply.github.com>
2025-02-04 18:27:33 -0500

    fix: exercise dao works now

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -1,6 +1,5 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-import 'package:benchy/database/models.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
